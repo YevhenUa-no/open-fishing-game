2b3edb1d6554a2ba144eba891aad1c99e6feb8d8
YevhenUa-no <127596731+YevhenUa-no@users.noreply.github.com>
2026-07-15 21:26:30 +0200

    Remove trailing slash from SERVER_URL

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 // After you deploy the /server folder (e.g. to Render or Fly.io),
 // paste its public URL here. This is the ONLY thing you need to change
 // before deploying the client to GitHub Pages.
-export const SERVER_URL = "https://open-fishing-game.onrender.com/";
+export const SERVER_URL = "https://open-fishing-game.onrender.com";
